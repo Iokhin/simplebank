044695b3877e6aaf6e18757149a0dbffeaa45707
zxc <zxc@mail.com>
2025-06-14 15:55:08 +0300

    POST /users/login

--- api/server.go
+++ api/server.go
@@ -1,25 +1,37 @@
 package api
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	"log"
 	db "simplebank/db/sqlc"
+	"simplebank/token"
+	"simplebank/util"
 )
 
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
+	config     util.Config
 }
 
 func (server *Server) Start(address string) error {
 	return server.router.Run(address)
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	maker, err := token.NewPasetoMaker(config.SymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create tokenmaker: %w", err)
+	}
+	server := &Server{
+		store:      store,
+		tokenMaker: maker,
+		config:     config,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		err := v.RegisterValidation("currency", validCurrency)
@@ -28,7 +40,16 @@ func NewServer(store db.Store) *Server {
 		}
 	}
 
+	server.setUpRouter()
+
+	return server, nil
+}
+
+func (server *Server) setUpRouter() {
+	router := gin.Default()
+
 	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
@@ -40,7 +61,6 @@ func NewServer(store db.Store) *Server {
 	router.POST("/transfers", server.transfer)
 
 	server.router = router
-	return server
 }
 
 func errorResponse(err error) gin.H {
--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	_ "github.com/lib/pq"
 	"log"
 	"simplebank/api"
-	"simplebank/db/sqlc"
+	db "simplebank/db/sqlc"
 	"simplebank/util"
 )
 
@@ -19,9 +19,11 @@ func main() {
 		log.Fatal("cannot connect to db", err)
 	}
 
-	store := sqlc.NewStore(conn)
-	server := api.NewServer(store)
-
+	store := db.NewStore(conn)
+	server, err := api.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
 	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
